b76b3b46775b2296025557c7cc9e406036f0dc46
Will00000000 <rm94807@estudante.fieb.edu.br>
2025-10-09 07:28:38 -0300

    V5.05_W
    Renomeando prefabs

--- New Alleanza/Assets/Fase 1/Scripts/HUD.cs
+++ New Alleanza/Assets/Fase 1/Scripts/HUD.cs
@@ -6,30 +6,36 @@ public class HUD : MonoBehaviour
 {
     [SerializeField] GameObject mapa;
     [SerializeField] GameObject inventario;
+    [SerializeField] GameObject UI;
 
     private void Start()
     {
         mapa.SetActive(false);
         inventario.SetActive(false);
+        UI.SetActive(true);
     }
 
     public void MapaAbre()
     {
         mapa.SetActive(true);
+        UI.SetActive(false);
     }
 
     public void MapaFecha()
     {
         mapa.SetActive(false);
+        UI.SetActive(true);
     }
 
     public void InventarioAbre()
     {
         inventario.SetActive(true);
+        UI.SetActive(false);
     }
 
     public void InventarioFecha()
     {
         inventario.SetActive(false);
+        UI.SetActive(true);
     }
 }
