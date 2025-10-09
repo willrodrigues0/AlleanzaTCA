9928c0150bd7347ae23d671733788ee949c30a4e
Luisgsx7 <rm94680@estudante.fieb.edu.br>
2025-10-09 08:11:27 -0300

    V5.07
    Interface completamente estruturada.

--- New Alleanza/Assets/Fase 1/Scripts/HUD.cs
+++ New Alleanza/Assets/Fase 1/Scripts/HUD.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class HUD : MonoBehaviour
 {
+    //SISTEMA DE FERRAMENTAS
     [SerializeField] GameObject mapa;
     [SerializeField] GameObject inventario;
     [SerializeField] GameObject UI;
 
-    private void Start()
+    private void Awake()
     {
         mapa.SetActive(false);
         inventario.SetActive(false);
@@ -37,5 +39,49 @@ public class HUD : MonoBehaviour
     {
         inventario.SetActive(false);
         UI.SetActive(true);
+    }
+
+    //SISTEMA PAUSE
+    public GameObject PauseMenu;
+    public GameObject ConfigMenu;
+    public bool isPause;
+
+    void Start()
+    {
+        PauseMenu.SetActive(false);
+    }
+
+    public void PauseGame()
+    {
+        PauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+
+        UI.SetActive(false);
+    }
+
+    public void ResumeGame()
+    {
+        PauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+
+        UI.SetActive(true);
+    }
+
+    public void AbrirMenuConfig ()
+    {
+        ConfigMenu.SetActive(true);
+        PauseMenu.SetActive(false);
+    }
+
+    public void FechaManuConfig ()
+    {
+        ConfigMenu.SetActive (false);
+        PauseMenu.SetActive (true);
+    }
+
+    public void Sair ()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("MenuPrincipal");
     }
 }
