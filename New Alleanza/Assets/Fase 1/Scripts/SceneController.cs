e42b77f48962c7658e19662bb9a0d7933745f133
Will00000000 <rm94807@estudante.fieb.edu.br>
2025-11-03 08:33:15 -0300

    Revert "V06.08_W"
    This reverts commit 2

--- New Alleanza/Assets/Fase 1/Scripts/SceneController.cs
+++ New Alleanza/Assets/Fase 1/Scripts/SceneController.cs
@@ -3,12 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
-    float alturaCam = 2;
-    
-    //[SerializeField] GameObject visao_cam;
-
-    //bool piso_1 = false;
-    //bool ponteCam = false;
+    public GameObject cam;
 
     public void GoPraia()
     {
@@ -20,29 +15,16 @@ public class SceneController : MonoBehaviour
         SceneManager.LoadScene("MorganHouse");
     }
 
-    void OnCollisionEnter2D(Collision2D col)
+    void Update ()
     {
-        if (gameObject.tag == "chao" && col.gameObject.tag == "Player")
-        {
-            Camera.main.transform.position = new Vector3(col.transform.position.x, col.transform.position.y + alturaCam, Camera.main.transform.position.z);
-        }
-
-        if (gameObject.tag == "ponte" && col.gameObject.tag == "Player")
-        {
-            Camera.main.transform.position = new Vector3(col.gameObject.transform.position., col.transform.position.y + alturaCam, Camera.main.transform.position.z);
-        }
+        
     }
     
-    /*void VisaoCam ()
+    /*void OnColliderEnter2D (Collider2D col)
     {
-        if (ponteCam)
+        if (col.gameObject.tag == "Player")
         {
-            Camera.main.transform.position = new Vector3(visao_cam.transform.position.x, visao_cam.transform.position.y, Camera.main.transform.position.z);
-        }
-
-        if (piso_1)
-        {
-            Camera.main.transform.position = new Vector3 ()
+            cam.transform.position = new Vector3(transform.position.x, transform.position.y + 2, transform.position.z);
         }
     }*/
 }
